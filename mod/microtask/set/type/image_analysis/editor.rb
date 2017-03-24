d5a8f039fdb63f98e6937801eb58e3ef7d941296
Srivigneshwar <prasadsrivi@gmail.com>
2017-03-24 03:09:47 +0000

    no fix

--- mod/microtask/set/type/image_analysis/editor.rb
+++ mod/microtask/set/type/image_analysis/editor.rb
@@ -1,10 +1,10 @@
 format :html do
-  view :content_formgroup do |args|
+  view :content_formgroup, cache: :never do |args|
     image_name = card.name
     image_url = 'https://wagntest.s3.amazonaws.com/files/sample_images/'
     image_url += image_name
     load_image = image_tag(image_url, class: 'profile_image')
-    card_structure = card.content
+    card_structure = card.template.db_content
     card_name = image_name.parameterize.underscore
     card_structure.scan(/\{\{([^\}]*)\}\}/).each do |incs|
       incs = incs[0].to_s.split('|', 2)[0]
