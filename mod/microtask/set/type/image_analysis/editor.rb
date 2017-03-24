59ded5e6c5c71a5caf54dcdfed7725d4994f5597
Srivigneshwar <prasadsrivi@gmail.com>
2017-03-24 02:21:46 +0000

    fix fix fix fix fix of radio button fix

--- mod/microtask/set/type/image_analysis/editor.rb
+++ mod/microtask/set/type/image_analysis/editor.rb
@@ -1,15 +1,16 @@
 format :html do
-  view :content_formgroup do |_args|
+  view :content_formgroup do |args|
     image_name = card.name
     image_url = 'https://wagntest.s3.amazonaws.com/files/sample_images/'
     image_url += image_name
     load_image = image_tag(image_url, class: 'profile_image')
-    card_structure = card.structure.content
+    card_structure = card.content
+    card_name = image_name.parameterize.underscore
     card_structure.scan(/\{\{([^\}]*)\}\}/).each do |incs|
       incs = incs[0].to_s.split('|', 2)[0]
       subcard = incs.split('+', 2)[1]
       nest_regex = /\{\{\+#{subcard}\|([^\}]*)\}\}/
-      css_attr = card.name.parameterize.underscore + subcard.parameterize.underscore
+      css_attr = card_name + subcard.parameterize.underscore
       field_html = field_nest(subcard, new: {})
       field_html =
         field_html.gsub 'pointer-radio-no', 'pointer-radio-no' + css_attr
@@ -18,7 +19,7 @@ format :html do
       card_structure.gsub! nest_regex, field_html
     end
     super_content =
-      content_tag(:div, card_structure.html_safe, class: 'main-form-content')
+      content_tag(:div, card_structure.html_safe, class: 'main-form-content')+
     content_tag(:div, load_image, class: 'image-container') + super_content
   end
 end
