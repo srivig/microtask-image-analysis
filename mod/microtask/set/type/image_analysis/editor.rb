c0071377086f0e52e1a2451f9ee22870901a18ed
Srivigneshwar <prasadsrivi@gmail.com>
2017-03-24 17:51:13 +0000

    fix fix fix fix fix of fix of dumb dumb code

--- mod/microtask/set/type/image_analysis/editor.rb
+++ mod/microtask/set/type/image_analysis/editor.rb
@@ -1,11 +1,12 @@
+
 format :html do
-  view :content_formgroup, cache: :never do |args|
+  view :content_formgroup, cache: :never do |_args|
     image_name = card.name
     image_url = 'https://wagntest.s3.amazonaws.com/files/sample_images/'
     image_url += image_name
     load_image = image_tag(image_url, class: 'profile_image')
     card_structure = card.template.db_content
-    card_name = image_name.parameterize.underscore
+    card_name = unique_id
     card_structure.scan(/\{\{([^\}]*)\}\}/).each do |incs|
       incs = incs[0].to_s.split('|', 2)[0]
       subcard = incs.split('+', 2)[1]
@@ -16,10 +17,10 @@ format :html do
         field_html.gsub 'pointer-radio-no', 'pointer-radio-no' + css_attr
       field_html =
         field_html.gsub 'pointer-radio-ye', 'pointer-radio-ye' + css_attr
-      card_structure.gsub! nest_regex, field_html
+      card_structure = card_structure.gsub nest_regex, field_html
     end
-    super_content =
-      content_tag(:div, card_structure.html_safe, class: 'main-form-content')+
-    content_tag(:div, load_image, class: 'image-container') + super_content
+    card_content =
+      content_tag(:div, card_structure.html_safe, class: 'main-form-content')
+    content_tag(:div, load_image, class: 'image-container') + card_content
   end
 end
