bec81f1519dbd6b5ec9fe636dc115d685c29e848
Srivigneshwar <prasadsrivi@gmail.com>
2017-03-26 14:22:07 +0200

    added delete script

--- db/migrate_scripts/generate_cards.rb
+++ db/migrate_scripts/generate_cards.rb
@@ -1,10 +1,20 @@
 require 'csv'
 
-Card::Auth.as_bot do
-  csv_text = File.read('db/migrate_scripts/data/sampleImageFileList.csv')
-  csv = CSV.parse(csv_text, :headers => false)
-  csv.each do |row|
-    file_name = row[0]
-    @c = Card.create! name: file_name, type: 'image_analysis'
+def create_image_cards
+  Card::Auth.as_bot do
+    csv_text = File.read('db/migrate_scripts/data/sampleImageFileList.csv')
+    csv = CSV.parse(csv_text, headers: false)
+    csv.each do |row|
+      file_name = row[0]
+      @c = Card.create! name: file_name, type: 'image_analysis'
+    end
+  end
+end
+
+def delete_image_cards
+  Card::Auth.as_bot do
+    existing_image_card_id = Card.search(type: 'image analysis', return: :id)
+    puts 'deleting #{existing_image_card_id.size} image_analysis cards'
+    Card.delete existing_image_card_id
   end
 end
