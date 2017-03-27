f6bb140b88c2d443ed9cbc1a91015a6b2d702510
Srivigneshwar <prasadsrivi@gmail.com>
2017-03-27 20:05:26 +0200

    changed image folder

--- mod/microtask/set/type/image_analysis/editor.rb
+++ mod/microtask/set/type/image_analysis/editor.rb
@@ -2,7 +2,7 @@
 format :html do
   view :content_formgroup, cache: :never do |_args|
     image_name = card.name
-    image_url = 'https://wagntest.s3.amazonaws.com/files/sample_images/'
+    image_url = 'https://wagntest.s3.amazonaws.com/files/images_to_classify/'
     image_url += image_name
     load_image = image_tag(image_url, class: 'profile_image')
     card_structure = card.template.db_content
